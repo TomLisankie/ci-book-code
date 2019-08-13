88e6b5e16dc1eb3033c0e0d3e44697ed74a20ea6
Thomas Lisankie <thomas@tomlisankie.com>
2019-08-13 18:00:53 -0400

    Scanner is 'complete' but does not compile

--- java/com/craftinginterpreters/Scanner.java
+++ java/com/craftinginterpreters/Scanner.java
@@ -159,4 +159,50 @@ class Scanner {
 	return source.charAt(current + 1);
     }
 
+    private void identifier () {
+	while (isAlphaNumeric (peek ())) {
+	    advance ();
+	}
+
+        String text = source.substring (start, current);
+
+	TokenType typeOfIdentifier = keywords.get (text);
+	if (typeOfIdentifier == null) {
+	    typeOfIdentifier = IDENTIFIER;
+	}
+	
+	addToken (typeOfIdentifier);
+    }
+
+    private boolean isAlpha (char c) {
+	return (c >= 'a'  && c <= 'z'
+	    || c >= 'A' && c <= 'Z'
+	    || c == '_');
+    }
+
+    private boolean isAlphaNumeric (char c) {
+	return isAlpha (c) || isDigit (c);
+    }
+
+    private static final HashMap <String, TokenType> keywords = new HashMap <> ();
+
+    static {
+	keywords.put ("and", AND);
+	keywords.put ("class", CLASS);
+	keywords.put ("else", ELSE);
+	keywords.put ("false", FALSE);
+	keywords.put ("for", FOR);
+	keywords.put ("fun", FUN);
+	keywords.put ("if", IF);
+	keywords.put ("nil", NIL);
+	keywords.put ("or", OR);
+	keywords.put ("print", PRINT);
+	keywords.put ("return", RETURN);
+	keywords.put ("super", SUPER);
+	keywords.put ("this", THIS);
+	keywords.put ("true", TRUE);
+	keywords.put ("var", VAR);
+	keywords.put ("while", WHILE);
+    }
+
 }
