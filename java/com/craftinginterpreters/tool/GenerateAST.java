bfd6e79d7aa6ad53ac9b161178085d1ba017c9e6
Thomas Lisankie <thomas@tomlisankie.com>
2019-08-15 14:36:10 -0400

    Finished code for creating a set of classes from a given set of expressions types

---
+++ java/com/craftinginterpreters/tool/GenerateAST.java
@@ -0,0 +1,77 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAST {
+    public static void main (String[] args) throws IOException {
+	if (args.length != 1) {
+	    System.err.println ("Usage: generate_ast <output_dir>");
+	    System.exit (1);
+	}
+	
+	String outputDir = args [0];
+
+	defineAST (outputDir, "Expr", Arrays.asList (
+						     "Binary : Expr left, Token operator, Expr right",
+						     "Grouping : Expr expression",
+						     "Literal : Object value",
+						     "Unary : Token operator, Expr right"
+						     ));
+    }
+
+    private static void defineAST (String outputDir, String baseName, List <String> types) throws IOException {
+	String path = outputDir + "/" + baseName + ".java";
+	PrintWriter writer = new PrintWriter (path, "UTF-8");
+
+	writer.println ("package com.craftinginterpreters.lox;");
+	writer.println ();
+	writer.println ("import java.util.List;");
+	writer.println ();
+	writer.println ("abstract class " + baseName + " {");
+
+	defineVisitor (writer, baseName, types);
+
+	for (String type : types) {
+	    String[] typeSplit = type.split (":");
+	    String className = typeSplit[0].trim ();
+	    String fields = typeSplit[1].trim ();
+	    defineType (writer, baseName, className, fields);
+	}
+
+	writer.println ("}");
+	writer.close ();
+    }
+
+    private static void defineType (PrintWriter writer, String baseName, String className, String fieldList) {
+	writer.println ("    static class " + className + " extends " + baseName + " {");
+
+	writer.println ();
+
+	String [] fields = fieldList.split (", ");
+	for (String field : fields) {
+	    writer.println ("        final " + field + ";");
+	}
+	
+	// Constructor
+	writer.println ("        " + className + " (" + fieldList + ") {");
+
+	// Store parameters in fields
+	for (String field : fields) {
+	    String fieldName = field.split (" ")[1];
+	    writer.println ("            this." + fieldName + " = " + fieldName + ";");
+	}
+
+	 writer.println("        }");
+
+	 writer.println("    }");
+	
+    }
+
+    private static void defineVisitor (PrintWriter writer, String baseName, List<String> types) {
+	
+    }
+    
+}
